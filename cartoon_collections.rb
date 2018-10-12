b3f9b6eceec248ddbfbd911a7eaaaf38b47280d1
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-12 15:29:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,14 @@
 def roll_call_dwarves(dwarf_names)# code an argument here
   # Your code here
-  
+  each_with_index {|item,name|
+   
+   
+ } 
+
 end
+%w(cat dog wombat).each_with_index { |item, index|
+  hash[item] = index
+}
 
 def summon_captain_planet# code an argument here
   # Your code here
