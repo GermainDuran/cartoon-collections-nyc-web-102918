3e57cb32a4b49246a91afaaa6f89ab73e1362e13
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-12 16:18:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
- if array.include?cheese_types == true
+  array.include?cheese_types == true
    return array.first
   
 end
